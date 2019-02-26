fcd5d5648b6225888056121a8e44ec6d32bdc978
Vitali-Zhura <vitosha84@tut.by>
2019-02-26 23:23:50 +0300

    delete package src

--- src/main/java/com/epam/brest/courses/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.epam.brest.courses;
-
-import java.math.BigDecimal;
-
-public interface Calculator {
-
-    BigDecimal calculate (BigDecimal distance, BigDecimal tarifDistance, BigDecimal tarifWeight);
-}
